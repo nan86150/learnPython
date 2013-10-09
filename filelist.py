dc55c06e701fea39831e89b4b0defd36f6f8bfcc
nan86150 <sn_1991@163.com>
2013-10-09 16:00:32 +0800

    make the filelist.py perfect

--- filelist.py
+++ filelist.py
@@ -3,7 +3,11 @@
 import os
 import os.path
 
-rootdir = "C:\\"
+rootdir = "E:\WorkSpace\python"
+global filenum
+filenum = 0
+global dirnum
+dirnum = 0
 
 def getDirList( path ):
 	path = str( path )
@@ -27,37 +31,92 @@ def getFileList( path ):
 	result = [x for x in list if os.path.isfile( path+x ) ]
 	return result
 
-def test(path, xx):
+def humSize( size ):
+	if size/(2**30):
+		return "%.2fGB" %(size/float(2**30))
+	elif size/(2**20):
+		return "%.2fMB" %(size/float(2**20))
+	elif size/(2**10):
+		return "%.2fKB" %(size/float(2**10))
+	else:
+		return "%sB" %(size)
+		
+def test(path, offsets):
+		global dirnum, filenum
+		dirnum += 1
 		filelist = getFileList(path)
 		dirlist = getDirList(path)
 		dir = os.path.abspath(path)
-		print "<br>" + xx + dir
-		fp.write("<br>" + xx + dir + "\n")
+		print "<br>\t%s %s" %(offsets,path)
+		fp.write("<br>\t\t\t%s %s\n" %(offsets,os.path.basename(path)) )
+		offsets = "│   " + offsets
 		
 		for file in filelist:
-			print "<br>" + xx + file + "  " + str(os.path.getsize(dir+"\\"+file))
-			fp.write("<br>" + "|--" + xx + file + "\n" + "  " + str(os.path.getsize(dir+"\\"+file)) )
-		print ""
-		xx = xx + "  "
+			print "<br>%s %s (%s)\n" %(offsets, file, humSize(os.path.getsize(dir+"\\"+file)))
+			fp.write("<br>\t\t\t%s %s (%s)\n" %(offsets, file, humSize(os.path.getsize(dir+"\\"+file))))
+			filenum += 1
+				
+		dir = os.path.abspath(path)
 		for x in dirlist:
-			test(dir+"\\"+x, xx) 
+			test(dir+"\\"+x, offsets) 
 	
 
-	
+head = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<style type="text/css">
+			<!-- 
+			BODY { font-family : courier, monospace, sans-serif; }
+			P { font-weight: normal; font-family : courier, monospace, sans-serif; color: black; background-color: transparent;}
+			B { font-weight: normal; color: black; background-color: transparent;}
+			A:visited { font-weight : normal; text-decoration : none; background-color : transparent; margin : 0px 0px 0px 0px; padding : 0px 0px 0px 0px; display: inline; }
+			A:link    { font-weight : normal; text-decoration : none; margin : 0px 0px 0px 0px; padding : 0px 0px 0px 0px; display: inline; }
+			A:hover   { color : #000000; font-weight : normal; text-decoration : underline; background-color : yellow; margin : 0px 0px 0px 0px; padding : 0px 0px 0px 0px; display: inline; }
+			A:active  { color : #000000; font-weight: normal; background-color : transparent; margin : 0px 0px 0px 0px; padding : 0px 0px 0px 0px; display: inline; }
+			.VERSION { font-size: small; font-family : arial, sans-serif; }
+			.NORM { color: black; background-color: transparent;}
+			.FIFO { color: purple; background-color: transparent;}
+			.CHAR { color: yellow; background-color: transparent;}
+			.DIR  { color: blue; background-color: transparent;}
+			.BLOCK { color: yellow; background-color: transparent;}
+			.LINK { color: aqua; background-color: transparent;}
+			.SOCK { color: fuchsia; background-color: transparent;}
+			.EXEC { color: green; background-color: transparent;}
+			-->
+		</style>
+	</head>
+	<body>
+			<p>文件列表
+'''
+# tail = '''
+		# <br><br>
+		# </p>
+		# <p>
 
-# dir = os.path.abspath("E:\WorkSpace\python")
-# print dir
-# dir = dir + "\\" + "hh"
-# print dir
+		# <br><br>
+		# </p>
+	# </body>
+# </html>
+# '''
+tail = '''
+	</body>
+</html>
+'''
+
 
 fp = open("filelist.html","w")
-fp.write("<html>" + "\n")
-print test("E:\WorkSpace\python", "|--")
-fp.close
-print os.path.getsize("file.py")
-os.system("pause")
+fp.write(head)
 
-# print "Dir"	
-# print getDirList(rootdir)
-# print "File"	
-# print getFileList(rootdir)
+test(rootdir, "├──")
+fp.write("</p><p>%d个文件夹，%d个文件<br><br></p>" %(dirnum, filenum))
+fp.write(tail)
+fp.close
+
+print filenum
+print dirnum
+
+fp = open("filelist.html","r")
+fp.readline()
+fp.close
+
